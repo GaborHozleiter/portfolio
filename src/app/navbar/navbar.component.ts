e9f7da84c2426e413e57ca33c471e08cedf33826
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-06-04 22:48:21 +0200

    continuation of the construction of the navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+    openedMenu: boolean = false;
 
+    openMenu(){
+      this.openedMenu = true;
+    }
+
+    closeMenu(){
+      this.openedMenu = false;
+    }
+
+    borderAnimation(){
+      alert('sfhx');
+    }
 }
