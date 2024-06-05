41346c0ae228e71a2b4be5420359fdba82abdb17
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-06-06 00:11:59 +0200

    continuation of the construction of the landing-page component

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -8,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent implements OnInit {
+    
+    arrowNumber:number = 3;
+    email:string = 'hozleitergabor@gmail.com';
+
+    constructor(){
+      //this.xy();
+    }
+
     ngOnInit(): void {
+      
       
     }
 
-    email:string = 'hozleitergabor@gmail.com';
-
+    xy(){
+      setInterval(() => {
+        if(this.arrowNumber < 6){
+          this.arrowNumber ++;
+        }else{
+          this.arrowNumber = 0;
+        }
+      }, 1000);
+    }
 }
