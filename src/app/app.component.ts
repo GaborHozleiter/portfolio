1e834a4ef0644a4f7670d48c99045f9c3802caea
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-06-06 22:14:29 +0200

    continuation of the construction of the about-me section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LandingPageComponent, AboutMeComponent],
+  imports: [RouterOutlet, NavbarComponent, LandingPageComponent, AboutMeComponent, MySkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
