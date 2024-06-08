f69e6af806c3d0ec9950bdb9c2b365dce6d1b796
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-06-09 00:51:04 +0200

    continuation of the construction of the portfolio-project component

--- src/app/portfolio-project/portfolio-project.component.ts
+++ src/app/portfolio-project/portfolio-project.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio-project',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './portfolio-project.component.html',
   styleUrl: './portfolio-project.component.scss'
 })
 export class PortfolioProjectComponent {
+    
+    containerOpacity = false;
+    
+  
+  animation1(){
+      this.containerOpacity = true;
+    }
 
+    animation2(){
+      this.containerOpacity = false;
+    }
 }
