1012c4b086b52fb10b3d7da68d0449fd3cf4079c
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-06-13 12:55:12 +0200

    continuation of the construction of the footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,13 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, LandingPageComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent,
-    ContactComponent],
+    ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
