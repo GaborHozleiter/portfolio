74dffb63b5413caebeaaa54513a06855bb00cab4
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-06-16 00:50:18 +0200

    added aslant arrow component to my-skills and portfolio component

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PortfolioProjectComponent } from '../portfolio-project/portfolio-project.component';
+import { ArrowAslantComponent } from '../arrow-aslant/arrow-aslant.component';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [ PortfolioProjectComponent ],
+  imports: [ PortfolioProjectComponent, ArrowAslantComponent ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
